a875dcdc952235e5a3d0939faef6b5ffde95c465
Frank Grove <fragro@gmail.com>
2011-12-18 14:03:08 -0600

    multisite

--- wp-config.php
+++ wp-config.php
@@ -78,8 +78,8 @@ define('WPLANG', '');
  */
 
 /* Multisite */
-define( 'MULTISITE', false );
-define( 'SUBDOMAIN_INSTALL', false );
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', true );
 $base = '/';
 define( 'DOMAIN_CURRENT_SITE', 'gatulsa-occupytulsa.dotcloud.com' );
 define( 'PATH_CURRENT_SITE', '/' );
